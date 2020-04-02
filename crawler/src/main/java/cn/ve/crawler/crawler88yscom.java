21b54a3f046caaa83ee78444f7f9e2f2ff9ad105
weiye <834250018@qq.com>
2020-04-03 01:51:35 +0800

    1.调整了下爬虫

--- crawler/src/main/java/cn/ve/crawler/crawler88yscom.java
+++ crawler/src/main/java/cn/ve/crawler/crawler88yscom.java
@@ -11,6 +11,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.springframework.util.StringUtils;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -35,25 +37,37 @@ public class crawler88yscom {
         // 创建HttpClient对象
         CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(cm).build();
         CloseableHttpResponse response = null;
-        Map<String, List> map = new HashMap<>();
-        for (int i = 1; i < 2; i++) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i = 1; i < 100; i++) {
             // 创建HttpGet对象,设置url访问地址
-            HttpGet httpGet = new HttpGet(HOST + "/vod-type-id-1-pg-" + i + ".html");
+            HttpGet httpGet = new HttpGet(HOST + "/vod-list-id-1-pg-" + i + "-order--by-hits-class-0-year-0-letter--area--lang-.html");
             response = httpClient.execute(httpGet);
             Document doc = Jsoup.parse(EntityUtils.toString(response.getEntity(), "utf8"));
             Elements elements = doc.select(".p1.m1 a");
-//            for (int j = 0; j < elements.size(); j++) {
-            for (int j = 0; j < 3; j++) {
+            for (int j = 0; j < elements.size(); j++) {
                 String title = elements.get(j).attr("title");
                 String href = elements.get(j).attr("href");
                 System.out.println(title + ":" + href);
                 map.put(title, analysisDetailPage(HOST + href));
             }
-            System.out.println();
-        }
-        if (response != null) {
             response.close();
         }
+        store(map);
+    }
+
+    private static void store(Map<String, List<String>> map) throws IOException {
+        System.out.println();
+        FileWriter fileWriter = new FileWriter("test.txt");
+        for (Map.Entry<String, List<String>> stringListEntry : map.entrySet()) {
+            fileWriter.write(stringListEntry.getKey());
+            fileWriter.write("\r\n");
+            for (String s : stringListEntry.getValue()) {
+                fileWriter.write(s);
+                fileWriter.write("\r\n");
+            }
+            fileWriter.write("\r\n");
+        }
+        fileWriter.close();
     }
 
     private static List<String> analysisDetailPage(String url) throws Exception {
@@ -89,7 +103,7 @@ public class crawler88yscom {
             List<String> result = new ArrayList<>();
             for (String u : urls) {
                 // 视频编码后的url
-                if (StringUtils.isEmpty(u) || u.length()< 10) {
+                if (StringUtils.isEmpty(u) || u.length() < 10) {
                     continue;
                 }
                 final String r = u.replaceAll("'", "")
